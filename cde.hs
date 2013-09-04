6b61de3b103103ff473bdfa03c9648c4ed66b8e6
Jean-Alexandre Peyroux <m@j4.pe>
2013-09-04 11:49:32 +0200

    add args

--- cde.hs
+++ cde.hs
@@ -1,9 +1,23 @@
+module Main where 
+
+import System.Environment
+import Text.Read
+
 data Devise = Euro Double
-	| Dollar Double
-	deriving (Eq, Show)
+            | Dollar Double
+            deriving (Show, Eq, Ord)
 
 cde :: Devise -> Devise
 cde a = case a of
-	Euro a -> Dollar $ a * devise
-	Dollar a -> Euro $ a / devise
-	where devise = 1.3383
+  Euro a -> Dollar $ a * devise
+  Dollar a -> Euro $ a / devise
+  where devise = 1.3166
+
+parseOpt x y 
+  | y == "d" = Dollar (read x:: Double)
+  | y == "e" = Euro (read x:: Double)
+  | otherwise = Euro 0
+
+main = do
+  [x, y] <- getArgs
+  putStrLn . show . cde $ parseOpt x y
